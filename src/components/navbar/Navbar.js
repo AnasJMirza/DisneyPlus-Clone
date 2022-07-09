4cf8d793248cced696cdf06750752722eadf4448
AnasJMirza <ajmhimself@gmail.com>
2022-07-09 06:27:50 +0500

    full login functionality with redux-toolkit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,57 +1,64 @@
-import React from 'react';
-import './Navbar.css';
-import useNavbar from './useNavbar';
+import React from "react";
+import "./Navbar.css";
+import useNavbar from "./useNavbar";
 
-import logo from '../../assets/images/logo.svg'
-import home from '../../assets/images/home-icon.svg'
-import search from '../../assets/images/search-icon.svg'
-import watchlist from '../../assets/images/watchlist-icon.svg'
-import orignals from '../../assets/images/original-icon.svg'
-import movies from '../../assets/images/movie-icon.svg'
-import series from '../../assets/images/series-icon.svg'
+import logo from "../../assets/images/logo.svg";
+import home from "../../assets/images/home-icon.svg";
+import search from "../../assets/images/search-icon.svg";
+import watchlist from "../../assets/images/watchlist-icon.svg";
+import orignals from "../../assets/images/original-icon.svg";
+import movies from "../../assets/images/movie-icon.svg";
+import series from "../../assets/images/series-icon.svg";
 
 const Navbar = () => {
-    const { handleAuth } = useNavbar();
-    return (
-        <div className='navbar-body'>
-            <div className='navbar-logo'>
-                <img src={logo} alt="Disney+" />
-            </div>
+  const { handleAuth, userName, userPhoto } = useNavbar();
 
-            <div>
-                <ul className='navigation'>
-                    <div className='navigation-link-wrapper'>
-                        <img src={home} alt='home' className='icon-image'/>
-                        <li className='navigation-link'>Home</li>
-                    </div>
-                    <div className='navigation-link-wrapper'>
-                        <img src={search} alt='search' className='icon-image'/>
-                        <li className='navigation-link'>Search</li>
-                    </div>
-                    <div className='navigation-link-wrapper'>
-                        <img src={watchlist} alt='watchlist' className='icon-image'/>
-                        <li className='navigation-link'>Watchlist</li>
-                    </div>
-                    <div className='navigation-link-wrapper'>
-                        <img src={orignals} alt='orignals' className='icon-image'/>
-                        <li className='navigation-link'>Orignals</li>
-                    </div>
-                    <div className='navigation-link-wrapper'>
-                        <img src={movies} alt='movies' className='icon-image'/>
-                        <li className='navigation-link'>Movies</li>
-                    </div>
-                    <div className='navigation-link-wrapper'>
-                        <img src={series} alt='series' className='icon-image'/>
-                        <li className='navigaion-link'>Series</li>
-                    </div>
-                </ul>
-            </div>
+  return (
+    <div className="navbar-body">
+      <div className="navbar-logo">
+        <img src={logo} alt="Disney+" />
+      </div>
 
-            <div className='navbar-login-button' onClick={handleAuth}>
-                Login
-            </div>
-        </div>
-    );
+      {!userName ? 
+        <div className="navbar-login-button" onClick={handleAuth}> Login </div>
+        : 
+        <>
+          <div>
+            <ul className="navigation">
+              <div className="navigation-link-wrapper">
+                <img src={home} alt="home" className="icon-image" />
+                <li className="navigation-link">Home</li>
+              </div>
+              <div className="navigation-link-wrapper">
+                <img src={search} alt="search" className="icon-image" />
+                <li className="navigation-link">Search</li>
+              </div>
+              <div className="navigation-link-wrapper">
+                <img src={watchlist} alt="watchlist" className="icon-image" />
+                <li className="navigation-link">Watchlist</li>
+              </div>
+              <div className="navigation-link-wrapper">
+                <img src={orignals} alt="orignals" className="icon-image" />
+                <li className="navigation-link">Orignals</li>
+              </div>
+              <div className="navigation-link-wrapper">
+                <img src={movies} alt="movies" className="icon-image" />
+                <li className="navigation-link">Movies</li>
+              </div>
+              <div className="navigation-link-wrapper">
+                <img src={series} alt="series" className="icon-image" />
+                <li className="navigaion-link">Series</li>
+              </div>
+            </ul>
+          </div>
+
+          <div >
+            <img src={userPhoto} alt="Profile"  className="profile-pic" />
+          </div>
+        </>
+      }
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/navbar/useNavbar.js
+++ src/components/navbar/useNavbar.js
@@ -1,37 +1,43 @@
-import { async } from '@firebase/util';
-import React from 'react';
-import { auth, provider } from '../../config/Firebase';
-import { signInWithPopup } from 'firebase/auth'
+import { async } from "@firebase/util";
+import React from "react";
+import { auth, provider } from "../../config/Firebase";
+import { signInWithPopup } from "firebase/auth";
 
-import { selectUserName, selectUserEmail, selectUserphoto, setUserLoginDetails } from '../../features/auth/userSlice'
-import { useDispatch, useSelector } from 'react-redux';
-
+import {
+  selectUserName,
+  selectUserEmail,
+  selectUserphoto,
+  setUserLoginDetails,
+} from "../../features/auth/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const useNavbar = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  const userEmail = useSelector(selectUserEmail);
+  const userPhoto = useSelector(selectUserphoto);
 
-    const dispatch = useDispatch()
-    const userName = useSelector(selectUserName)
-    const userEmail = useSelector(selectUserEmail)
-    const userPhoto = useSelector(selectUserphoto)
-
-    const handleAuth = async () => {
-        try {
-            
-            const result = await signInWithPopup(auth, provider)
-            dispatch(setUserLoginDetails({
-                name: result.user.name,
-                email: result.user.email,
-                photo: result.user.photoURL,
-            }))
-
-        } catch (error) {
-            console.log(error.message);
-        }
+  const handleAuth = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log(result.user.photoURL);
+      dispatch(
+        setUserLoginDetails({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        })
+      );
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 
-    return {
-        handleAuth
-    }
+  return {
+    handleAuth,
+    userName,
+    userPhoto
+  };
 };
 
 export default useNavbar;
