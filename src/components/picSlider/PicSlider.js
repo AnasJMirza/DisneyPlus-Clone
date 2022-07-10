67834548bf8df4993445ad91e150acc80abc483e
AnasJMirza <ajmhimself@gmail.com>
2022-07-11 02:53:30 +0500

    works on slider

--- src/components/picSlider/PicSlider.js
+++ src/components/picSlider/PicSlider.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./PicSlider.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+
+import slider1 from "../../assets/images/slider-badag.jpg";
+import slider2 from "../../assets/images/slider-badging.jpg";
+import slider3 from "../../assets/images/slider-scale.jpg";
+import slider4 from "../../assets/images/slider-scales.jpg";
 
 const PicSlider = () => {
   let settings = {
@@ -11,16 +17,28 @@ const PicSlider = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
+    centerMode: true,
+    adaptiveHeight: true,
   };
 
   return (
-    <div>
-        <Slider {...settings}>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-        </Slider>
-    </div>
+    <Slider {...settings} className="slider">
+      <div className="slider-image-wrapper">
+        <img src={slider1} alt="" className="slider-image" />
+      </div>
+
+      <div className="slider-image-wrapper">
+        <img src={slider2} alt=""  className="slider-image"/>
+      </div>
+
+      <div className="slider-image-wrapper">
+        <img src={slider3} alt="" className="slider-image" />
+      </div>
+
+      <div className="slider-image-wrapper">
+        <img src={slider4} alt="" className="slider-image" />
+      </div>
+    </Slider>
   );
 };
 
