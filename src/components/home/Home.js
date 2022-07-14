603df6d8886629cf38732dba1945be41536fac74
AnasJMirza <ajmhimself@gmail.com>
2022-07-14 16:11:49 +0500

    deployment settings

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,18 +5,7 @@ import PicSlider from "../picSlider/PicSlider";
 import Viewr from "../Viewr/Viewr";
 import MovieGrid from "../movieGrid/MovieGrid";
 
-import viewr1 from "../../assets/images/viewers-disney.png";
-import viewr2 from "../../assets/images/viewers-marvel.png";
-import viewr3 from "../../assets/images/viewers-national.png";
-import viewr4 from "../../assets/images/viewers-pixar.png";
-import viewr5 from "../../assets/images/viewers-starwars.png";
-
-import { useEffect } from "react";
-import { selectUserName } from "../../features/auth/userSlice";
-import db from "../../config/Firebase";
-import { collection, getDocs } from "firebase/firestore";
-import { useDispatch, useSelector } from "react-redux";
-import { setMovies } from "../../features/movie/movieSlice";
+import { useSelector } from "react-redux";
 import {
   selectRecomended,
   selectNewDisney,
